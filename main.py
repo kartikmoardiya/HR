7bb550a83b375968adc90977f11e7d96151ae1fb
Kartik Kishorbhai Moradiya <kartikmoradiya5964@gmail.com>
2025-02-18 17:31:22 +0530

    third

--- main.py
+++ main.py
@@ -71,5 +71,8 @@ def main():
     # Run FastAPI with Uvicorn
     uvicorn.run(fastapi_app, host="0.0.0.0", port=8000)
 
+# Vercel expects this variable to run FastAPI
+app = fastapi_app
+
 if __name__ == "__main__":
     main()
